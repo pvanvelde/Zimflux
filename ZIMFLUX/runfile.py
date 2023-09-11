6198274a5cba9e36a23af91792627d307a74a477
pvanvelde <vanveldepieter@gmail.com>
2023-09-10 22:45:24 -0400

    Update readme

--- ZIMFLUX/runfile.py
+++ ZIMFLUX/runfile.py
@@ -1,11 +1,10 @@
 from photonpy.simflux import ZimfluxProcessor
 from photonpy import Gauss3D_Calibration
-import numpy as np
-import torch
-import pickle
 import matplotlib.pyplot as plt
+import sys
+sys.path.insert(1, './ZIMFLUX')
 from plot_figure_nanorulers import plot_nanorulerclusters
-
+import os
 # -------------------------------- User inputs --------------------------------------
 cfg = {
    'roisize':16,
@@ -27,29 +26,23 @@ cfg = {
     'depth': 60
 }
 
+# absolute path to data
+path = 'E:/Zimflux/3Dnanorulers/nanorulers_simflux_10ms_jan13_MMStack_Default.ome.tif'
+
+# absolute path to gain and offset data
+cfg['gain'] = 'E:/Zimflux/calibration/bright_2_MMStack_Default.ome.tif'
+cfg['offset'] = 'E:/Zimflux/calibration/dark_1_MMStack_Default.ome.tif'
+
+
 # calibration for astigmatic PSF to make initial guess
-calib_fn = 'E:/SIMFLUX Z/astigmatism_cal_100mlam_1/astigmatist_cal_100mlam_1_MMStack_Default.ome_gausscalib20nm.yaml'
+calib_fn = os.path.dirname(os.path.realpath('__file__'))+ '/astigmatist_gausscalib20nm.yaml'
 calib = Gauss3D_Calibration.from_file(calib_fn)
 cfg['psf_calib'] = calib_fn
 
-# folder name with data
-folder = 'nanorulers_simflux_10ms_originalanglejan13_2_firstpattern'
-
-# absolute path to data
-path = 'E:/SIMFLUX Z/nanorulers_simflux_10ms_originalanglejan13_2_firstpattern/nanorulers_simflux_10ms_originalanglejan13_2_firstpattern_MMStack_Default.ome.tif'
-
-# Output path for figures/results
-output_folder = 'E:/results_zimflux/'
-
-# absolute path to gain and offset data
-cfg['gain'] = 'E:/SIMFLUX Z/bright_10ms_oriangle/bright_2_MMStack_Default.ome.tif'
-cfg['offset'] = 'E:/SIMFLUX Z/dark_10ms_oriangle/dark_1_MMStack_Default.ome.tif'
-
 # intial wavevector in axial direction and number of iterations refinement
 init_kz = 13.2
-num_iterations = 10
-
-# -------------------------------- end --------------------------------------
+num_iterations = 20
+# -------------------------------- end user inputs --------------------------------------
 
 # Detect spots
 sp = ZimfluxProcessor(path, cfg)
@@ -65,9 +58,8 @@ plt.show()
 sp.estimate_angles(1, pitch_minmax_nm=[400,500], dft_peak_search_range=0.004)
 
 # refine axial pitch
-
 sp.refine_kz(init_kz,num_iterations)
-
+#
 # estimate phases
 sp.estimate_phases(10, iterations=10)
 
@@ -83,10 +75,7 @@ sp.sum_ds_filtered_undrift.applyDrift(sp.drift)
 sp.sum_ds_filtered_undrift.save(sp.resultsdir + "smlm_undrift" +".hdf5")
 
 # find cluster data
-plot_data= sp.cluster_picassopicksv2(pattern=0, depth=sp.depth,drift_corrected=True, fit_gaussians=False)
-clusterdata = open(sp.resultsdir+'clusterdata' , 'wb')
-pickle.dump(plot_data, clusterdata)
-clusterdata.close()
+clusterdata= sp.cluster_picassopicksv2(drift_corrected=True)
 
 # plot clusterdata
 plot_nanorulerclusters(clusterdata,sp.resultsdir)
